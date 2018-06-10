e26e56ac33fce7c61a08d0b3c38d46cc11aff7b7
GrossGration <sashamiko@bk.ru>
2018-06-10 11:22:22 +0500

    First push

---
+++ Assets/Scripts/TeleportToPosition.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportToPosition : MonoBehaviour {
+
+    public GameObject[] soundObjects;
+
+	public void MovePlayer()
+    {
+        Player.SetPosition(transform.position);
+        if (gameObject.GetComponent<Collider>().name == "Point_8")
+        {
+            soundObjects[0].GetComponent<AudioSource>().Stop();
+            gameObject.GetComponent<AudioSource>().Play();
+        }
+        if (gameObject.GetComponent<Collider>().name == "Point_4")
+        {
+            soundObjects[1].GetComponent<AudioSource>().Stop();
+            gameObject.GetComponent<AudioSource>().Play();
+        }
+    }
+}
